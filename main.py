4a90451206aabad0ce695e75c5f9d969f936bba2
PSobrevals <67247574+PSobrevals@users.noreply.github.com>
2020-10-25 14:22:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+'''
+# Aixó ho eliminem, però així ho tenim més a mà ja que els hem de mirar totes 
+URL's de les dades evolutives per barris, demografia Barcelona:
 
+Naixements: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm'
+Defuncions: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/defu/evo/sexe03.htm'
+Immigrants: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/imi/evo/sexe03.htm'
+Emmigrants: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/emi/evo/sexe03.htm'
+Altes per omisió: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/omi/evo/sexe03.htm'
+Baixes per inscripció indeguda: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/baixes/evo/sexe03.htm'
+Canvis de nacionalitat: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/canvisnacio/evo/t03.htm'
 
+urls = ['https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/defu/evo/sexe03.htm',
+        'https://www.bcn.cat/estadistica/catala/dades/tdemo/imi/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/emi/evo/sexe03.htm', 
+        'https://www.bcn.cat/estadistica/catala/dades/tdemo/omi/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/baixes/evo/sexe03.htm',
+        'https://www.bcn.cat/estadistica/catala/dades/tdemo/canvisnacio/evo/t03.htm']
+'''
 if __name__ == '__main__':
     url = 'https://www.bcn.cat/estadistica/catala/dades/tdemo/index.htm'
     pag_web = requests.get(url)
