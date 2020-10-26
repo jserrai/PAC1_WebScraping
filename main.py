8c645cead896123371656650cda9402b27a06157
jordi <jserrai@uoc.edu>
2020-10-26 11:01:11 +0100

    missate

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ urls = ['https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm'
         'https://www.bcn.cat/estadistica/catala/dades/tdemo/canvisnacio/evo/t03.htm']
 '''
 if __name__ == '__main__':
-    url = 'https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm' # He canviat l'URL per als naixements.
+    url = 'https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm'
     pag_web = requests.get(url)
     soup= BeautifulSoup(pag_web.content)
     table = soup.find('table') # Per a obtenir el codi de la taula de la primera url.
@@ -27,15 +27,16 @@ if __name__ == '__main__':
     taula = [] # Inicialitzem la taula
     row_num = -1 # inicialitzem la fila en la que ens trobem
     for r in row: # Itenerm per totes les files
-   	row_num += 1 
-	taula.append([]) 
-	for c in r.find_all("td"): 
-		columna = c.get_text(strip=True)
-		taula[row_num].append(columna)
+        row_num += 1
+        taula.append([])
+        for c in r.find_all("td"):
+            columna = c.get_text(strip=True)
+            taula[row_num].append(columna)
 		
 df = pd.DataFrame(taula)
 df
-df.to_csv('/Users/paula/Desktop/Taula.csv', index = False) # Crea un df amb la taula dels naixements.
+#df.to_csv('/Users/paula/Desktop/Taula.csv', index = False) # Crea un df amb la taula dels naixements.
+df.to_csv('./Taula.csv', index = False) # Crea un df amb la taula dels naixements.
 
     # per mirar si recupera be la primera web
     # sha de canviar la url per cada una de les que hem de mirar
