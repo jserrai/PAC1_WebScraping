302d99af64917545f2628de197d2a020bfe3bc23
jordi <jserrai@uoc.edu>
2020-10-26 16:33:56 +0100

    missate

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ urls = ['https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm'
         'https://www.bcn.cat/estadistica/catala/dades/tdemo/imi/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/emi/evo/sexe03.htm', 
         'https://www.bcn.cat/estadistica/catala/dades/tdemo/omi/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/baixes/evo/sexe03.htm',
         'https://www.bcn.cat/estadistica/catala/dades/tdemo/canvisnacio/evo/t03.htm']
+
 '''
+
 if __name__ == '__main__':
     url = 'https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm'
+
     pag_web = requests.get(url)
     soup= BeautifulSoup(pag_web.content)
     table = soup.find('table') # Per a obtenir el codi de la taula de la primera url.
@@ -32,12 +35,12 @@ if __name__ == '__main__':
         for c in r.find_all("td"):
             columna = c.get_text(strip=True)
             taula[row_num].append(columna)
-		
+
 df = pd.DataFrame(taula)
 df
 #df.to_csv('/Users/paula/Desktop/Taula.csv', index = False) # Crea un df amb la taula dels naixements.
 df.to_csv('./Taula.csv', index = False) # Crea un df amb la taula dels naixements.
 
-    # per mirar si recupera be la primera web
-    # sha de canviar la url per cada una de les que hem de mirar
+# per mirar si recupera be la primera web
+# sha de canviar la url per cada una de les que hem de mirar
 
