165bbc43cc028880ebeebb65ae88af47cc0a75a9
PSobrevals <67247574+PSobrevals@users.noreply.github.com>
2020-10-29 19:05:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,35 +11,46 @@ Immigrants: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/imi/evo/sexe03.h
 Emmigrants: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/emi/evo/sexe03.htm'
 Altes per omisió: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/omi/evo/sexe03.htm'
 Baixes per inscripció indeguda: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/baixes/evo/sexe03.htm'
-Canvis de nacionalitat: 'https://www.bcn.cat/estadistica/catala/dades/tdemo/canvisnacio/evo/t03.htm'
+'''
 
 urls = ['https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/defu/evo/sexe03.htm',
         'https://www.bcn.cat/estadistica/catala/dades/tdemo/imi/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/emi/evo/sexe03.htm', 
-        'https://www.bcn.cat/estadistica/catala/dades/tdemo/omi/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/baixes/evo/sexe03.htm',
-        'https://www.bcn.cat/estadistica/catala/dades/tdemo/canvisnacio/evo/t03.htm']
+        'https://www.bcn.cat/estadistica/catala/dades/tdemo/omi/evo/sexe03.htm', 'https://www.bcn.cat/estadistica/catala/dades/tdemo/baixes/evo/sexe03.htm']
 
-'''
+
 
 if __name__ == '__main__':
-    url = 'https://www.bcn.cat/estadistica/catala/dades/tdemo/naix/evo/sexe03.htm'
-
-    pag_web = requests.get(url)
-    soup= BeautifulSoup(pag_web.content)
-    table = soup.find('table') # Per a obtenir el codi de la taula de la primera url.
-    row = table.find_all("tr") # Per a trobar totes les files
-    taula = [] # Inicialitzem la taula
-    row_num = -1 # inicialitzem la fila en la que ens trobem
-    for r in row: # Itenerm per totes les files
-        row_num += 1
-        taula.append([])
-        for c in r.find_all("td"):
-            columna = c.get_text(strip=True)
-            taula[row_num].append(columna)
-
-df = pd.DataFrame(taula)
-df
-#df.to_csv('/Users/paula/Desktop/Taula.csv', index = False) # Crea un df amb la taula dels naixements.
-df.to_csv('./Taula.csv', index = False) # Crea un df amb la taula dels naixements.
+        
+    for i in urls:
+	
+	# Obtenim el URL:
+	url = i
+	
+	# Extraiem el titol del URL (Naix = Naixements, defu = defuncions, imi = imigrant, emi = emigrants, omi = omisió, baixes = baixes per inscripció indeguda o caducitat.
+	name = url.split('https://www.bcn.cat/estadistica/catala/dades/tdemo/' )
+	name = "".join(name)
+	name = name.split('/evo/sexe03.htm' )
+	name = "".join(name)
+	
+	# Extraiem les dades de la url:
+	pag_web = requests.get(url)
+	soup= BeautifulSoup(pag_web.content)
+	table = soup.find("table") # Per a obtenir el codi de la taula de la url.
+	row = table.find_all("tr") # Per a trobar totes les files
+	taula = [] # Inicialitzem la taula
+	row_num = -1 # Inicialitzem la fila en la que ens trobem
+	for r in row: # Iterem per totes les files
+		row_num += 1
+		taula.append([])
+		for c in r.find_all("td"):
+			columna = c.get_text(strip=True) # (!) Obtenim només el text
+			taula[row_num].append(columna)
+	
+	# Transformem i guardem les dades en forma de .csv
+	df = pd.DataFrame(taula)
+	file_csv = './' + name + '.csv'
+	df.to_csv(file_csv, index = False)
+        
 
 # per mirar si recupera be la primera web
 # sha de canviar la url per cada una de les que hem de mirar
