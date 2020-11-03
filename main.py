271d253c68c4d7f94d937faa92f74888f8abdcbd
PSobrevals <67247574+PSobrevals@users.noreply.github.com>
2020-11-03 18:57:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 #pip install webdriver_manager
 #pip install selenium
+#pip install more-itertools
 
 import requests
 import selenium
@@ -9,7 +10,7 @@ import csv
 import os
 import time
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+from more_itertools import split_after
 #from selenium.webdriver.common.keys import Keys
 
 #Instalem el driver:
@@ -21,43 +22,53 @@ driver.get(url) # Obrim la pg web a fer scraping.
 # XPath del nom dels paisos:'//*[@id="leftColumn"]//h2'
 paisos = driver.find_elements_by_xpath('//*[@id="leftColumn"]//h2')
 
-# XPath del titol de les taules:'//*[contains(@id,"indice_table_28")]/thead//tr/th'
+# XPath del titol de les taules:'//*[contains(@id,"indice_table_")]/thead//tr/th'
 rows_title = driver.find_elements_by_xpath('//*[@id="indice_table_28"]/thead/tr/th')
-# XPath del titol de les taules:'//*[contains(@id,"indice_table_")]/tbody//tr//td'
-rows_data = driver.find_elements_by_xpath('//*[contains(@id,"indice_table_")]/tbody//tr//td')
 
+#Inicialitzem les taules
 paisos_list = []
 rows_title_list = []
-rows_data_list = []
-
 
 for p in range(len(paisos)):
     paisos_list.append(paisos[p].text)
+
+
 for rt in range(len(rows_title)):
     rows_title_list.append(rows_title[rt].text)
-for rd in range(len(rows_data)):
-    rows_data_list.append(rows_data[rd].text)
 
-#diccionari = {rows_title_list[i]: rows_data_list[i] for i in range(len(rows_title_list))}
+rows_title_list.pop(0)
 
- currentDir = os.path.dirname(__file__)
-    filename = 'Index_investments_mundial.csv'
-    filePath = os.path.join(currentDir, filename)
+# Obtenim totes les id de les taules:
+leftCol = driver.find_elements_by_xpath('//*[contains(@id,"indice_table_")]')
+tables_id = []
+for i in leftCol:
+    tables_id.append(i.get_attribute('id'))
 
-    csvFile = open(filePath, 'wt', newline=' ')
-    writer = csv.writer(csvFile)
 
-    #anem fent fins que troba el final de row.
-    for p in paisos_list:
-        writer.writerow(p)
+rows_data_list = []
+
+currentDir = os.getcwd()
+filename = 'Index_investments_mundial.csv'
+filePath = os.path.join(currentDir, filename)
+
+csvFile = open(filePath, 'wt', newline='')
+writer = csv.writer(csvFile)
+
+c = 0
+try: 
+    for iid in tables_id:
+        pais = paisos_list.pop(c)
+        # XPath del titol de les taules:'//*[contains(@id,"indice_table_")]/tbody//tr//td'
+        writer.writerow(pais)
         writer.writerow(rows_title_list)
-        # A partir d'aqui no ho he canviat encara
-        try:
-            for c in rows_data_list:  # per cada cella
-                td = c.find_all('td')
-                row2 = [i.text.replace('\xa0','') for i in td]  # per eliminar el &nspb
-                writer.writerow(row2)
-        finally:
-            csvFile.close()  # si final de row
-        return 0
-
+        xpath = '//*[@id="' + iid + '"]/tbody//tr//td'
+        rows_data = driver.find_elements_by_xpath(xpath)
+        rows_data_list = []
+        for rd in range(len(rows_data)):
+            rows_data_list.append(rows_data[rd].text)
+        by_rows_data = list(split_after(rows_data_list, lambda x: x == ' '))
+        for row in by_rows_data:
+            writer.writerow(by_rows_data)
+        c += 1
+finally:
+    csvFile.close()
